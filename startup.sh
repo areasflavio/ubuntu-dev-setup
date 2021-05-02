5f2345b56c831bccd291ac88cf0489f3a78365c0
Flávio Arêas <vflavio9@gmail.com>
2021-05-02 13:19:22 -0300

    feat: add script

---
+++ startup.sh
@@ -0,0 +1,250 @@
+echo "Welcome! Let's start setting up your system. It could take more than 10 minutes, be patient"
+
+echo "What name do you want to use in GIT user.name?"
+echo "For example, mine will be \"Flávio Arêas\""
+read git_config_user_name
+
+echo "What email do you want to use in GIT user.email?"
+echo "For example, mine will be \"areasflavio@outlook.com\""
+read git_config_user_email
+
+echo "What is your github username?"
+echo "For example, mine will be \"areasflavio\""
+read username
+
+cd ~ && sudo apt-get update
+
+echo 'Installing curl' 
+sudo apt-get install curl -y
+
+echo 'Installing neofetch' 
+sudo apt-get install neofetch -y
+
+echo 'Installing latest git' 
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update && sudo apt-get install git -y
+
+echo 'Installing python3-pip'
+sudo apt-get install python3-pip -y
+
+echo 'Installing getgist to download dot files from gist'
+sudo pip3 install getgist
+export GETGIST_USER=$username
+
+if [$XDG_CURRENT_DESKTOP == 'KDE'] ; then
+    echo 'Cloning your Konsole configs from gist'
+    cd ~/.local/share/konsole && getmy OmniKonsole.profile && getmy OmniTheme.colorscheme
+
+    echo 'Installing Latte Dock'
+    sudo add-apt-repository ppa:kubuntu-ppa/backports -y
+    sudo apt-get update && sudo apt-get dist-upgrade
+    sudo apt-get install cmake extra-cmake-modules qtdeclarative5-dev libqt5x11extras5-dev libkf5iconthemes-dev libkf5plasma-dev libkf5windowsystem-dev libkf5declarative-dev libkf5xmlgui-dev libkf5activities-dev build-essential libxcb-util-dev libkf5wayland-dev git gettext libkf5archive-dev libkf5notifications-dev libxcb-util0-dev libsm-dev libkf5crash-dev libkf5newstuff-dev libxcb-shape0-dev libxcb-randr0-dev libx11-dev libx11-xcb-dev -y
+    sudo git clone https://github.com/KDE/latte-dock.git /usr/local/latte-dock
+    cd /usr/local/latte-dock && sudo sh install.sh
+
+    echo 'Installing Kvantum Manager'
+    sudo add-apt-repository ppa:papirus/papirus -y
+    sudo apt-get update && sudo apt install qt5-style-kvantum -y
+fi
+
+echo "Setting up your git global user name and email"
+git config --global user.name "$git_config_user_name"
+git config --global user.email $git_config_user_email
+
+echo 'Cloning your .gitconfig from gist'
+getmy .gitconfig
+
+echo 'Generating a SSH Key'
+ssh-keygen -t rsa -b 4096 -C $git_config_user_email
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub
+
+echo 'Installing ZSH'
+sudo apt-get install zsh -y
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s $(which zsh)
+
+echo 'Indexing snap to ZSH'
+sudo chmod 777 /etc/zsh/zprofile
+echo "emulate sh -c 'source /etc/profile.d/apps-bin-path.sh'" >> /etc/zsh/zprofile
+
+echo 'Installing Spaceship ZSH Theme'
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+source ~/.zshrc
+
+echo 'Installing FiraCode'
+sudo apt-get install fonts-firacode -y
+
+echo 'Installing NVM' 
+sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
+
+export NVM_DIR="$HOME/.nvm" && (
+git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+cd "$NVM_DIR"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+source ~/.zshrc
+clear
+
+echo 'Installing NodeJS LTS'
+nvm --version
+nvm install --lts
+nvm current
+
+echo 'Installing Yarn'
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install --no-install-recommends yarn
+echo '"--emoji" true' >> ~/.yarnrc
+
+echo 'Installing Typescript, AdonisJS CLI and Lerna'
+yarn global add typescript @adonisjs/cli lerna
+clear
+
+echo 'Installing VSCode'
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https -y
+sudo apt-get update && sudo apt-get install code -y
+
+
+echo 'Installing Vivaldi' 
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
+sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main' -y
+sudo apt update && sudo apt install vivaldi-stable
+
+echo 'Launching Vivaldi on Github so you can paste your keys'
+vivaldi https://github.com/settings/keys </dev/null >/dev/null 2>&1 & disown
+
+echo 'Installing Docker'
+sudo apt-get purge docker docker-engine docker.io
+sudo apt-get install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo chmod 777 /var/run/docker.sock
+
+echo 'Installing docker-compose'
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+echo 'Installing Heroku CLI'
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+heroku --version
+
+echo 'Installing Vercel CLI'
+sudo npm i -g vercel
+vercel --version
+
+echo 'Installing Netlify CLI'
+sudo npm install -g --unsafe-perm=true netlify-cli
+netlify --version
+
+
+echo 'Installing Insomnia Core and Omni Theme' 
+echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
+  | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
+  | sudo apt-key add -
+sudo apt-get update && sudo apt-get install insomnia -y
+mkdir ~/.config/Insomnia/plugins && cd ~/.config/Insomnia/plugins
+git clone https://github.com/Rocketseat/insomnia-omni.git omni-theme && cd ~
+
+echo 'Installing VLC'
+sudo apt-get install vlc -y
+sudo apt-get install vlc-plugin-access-extra libbluray-bdj libdvdcss2 -y
+
+echo 'Installing Discord'
+wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo dpkg -i discord.deb
+sudo apt-get install -f -y && rm discord.deb
+
+echo 'Installing Spotify' 
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update && sudo apt-get install spotify-client
+
+echo 'Installing Peek' 
+sudo add-apt-repository ppa:peek-developers/stable -y
+sudo apt update
+sudo apt install peek -y
+
+echo 'Installing CopyQ' 
+sudo add-apt-repository ppa:hluk/copyq -y
+sudo apt update
+sudo apt install copyq -y
+
+echo 'Installing Robo3t'
+sudo snap install robo3t-snap
+
+echo 'Updating and Cleaning Unnecessary Packages'
+sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get full-upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
+clear
+
+echo 'Installing postgres container'
+docker run --name database -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
+
+echo 'Installing mongodb container'
+docker run --name mongodb -p 27017:27017 -d -t mongo
+
+echo 'Installing redis container'
+docker run --name redis_skylab -p 6379:6379 -d -t redis:alpine
+clear
+
+echo 'Bumping the max file watchers'
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+echo 'Installing Beekeeper Studio'
+# Install our GPG key
+wget --quiet -O - https://deb.beekeeperstudio.io/beekeeper.key | sudo apt-key add -
+
+# add our repo to your apt lists directory
+echo "deb https://deb.beekeeperstudio.io stable main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list
+
+# Update apt and install
+sudo apt update
+sudo apt install beekeeper-studio
+
+echo 'Installing Mailspring'
+sudo snap install mailspring
+
+echo 'Installing Stacer'
+sudo add-apt-repository ppa:oguzhaninan/stacer
+sudo apt-get update
+sudo apt-get install stacer
+
+echo 'Installing Dropbox Client'
+wget https://linux.dropbox.com/packages/ubuntu/dropbox_1.6.0_amd64.deb
+sudo dpkg -i dropbox_1.6.0_amd64.deb
+
+echo 'Installing Ulauncher'
+sudo ppa:agornostal/ulauncher
+sudo apt install ./packagename.deb
+
+echo 'Installing Reactotron'
+wget https://github.com/infinitered/reactotron/releases/download/v3.0.0-beta.9/reactotron-app_3.0.0-beta.9_amd64.deb -O reactotron.appimage
+chmod +x reactotron.appimage
+sudo ./reactotron.appimage
+
+echo 'Installing Typora'
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+# add Typora's repository
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt-get update
+# install typora
+sudo apt-get install typora
+
+echo 'All setup, enjoy!'
